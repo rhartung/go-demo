43b5953b61ccbfdbcf4fa9f98c9e24f437038050
Robyn Lundin <robyn@hartung.com>
2017-11-07 21:36:34 -0800

    Adds variable declaration and string formatting

--- main.go
+++ main.go
@@ -5,7 +5,23 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, playground")
-	fmt.Println("I'm a change!")
-	fmt.Println("Uno mas!")
+
+	// Variable declaration and assignment - shorthand
+	// Variables accessible within function
+	a := "Oh hey there"
+	b := 4
+	c := true
+	d := 8.91
+
+	// %v used for string formatting w/variables
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+
+	// %T used for formatting string to show variable type
+	fmt.Printf("%T \n", a)
+	fmt.Printf("%T \n", b)
+	fmt.Printf("%T \n", c)
+	fmt.Printf("%T \n", d)
 }
